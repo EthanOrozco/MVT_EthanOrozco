89cc8abde2190e6bf0759fbb92c20eb1a3bc269d
Ethan Orozco <ethan.g.orozco@gmail.com>
2022-08-30 11:47:30 -0500

    Primer commit

---
+++ Familiares/views.py
@@ -0,0 +1,39 @@
+import datetime
+from urllib import request
+from django.shortcuts import render
+from Familiares.models import Familiar
+
+# Create your views here.
+def familiares(request):
+    familiar1=Familiar (nombre= "César Orozco", edad=43, cumpleaños=datetime.date(1979, 6, 19))
+    
+    
+    familiar2=Familiar (nombre= "Rocío Madrid", edad=43, cumpleaños=datetime.date(1979, 2, 3))
+    
+    
+    familiar3=Familiar (nombre= "Jair Orozco", edad=15, cumpleaños=datetime.date(2007, 2, 14))
+    
+
+    contexto = {
+        'familiar1': familiar1,
+        'familiar2': familiar2,
+        'familiar3': familiar3
+    }
+
+    return render(request, 'Familiares/familiares.html', contexto)
+
+def inicio(request):
+    
+    return render(request, 'index.html')
+
+def about(request):
+
+    return render(request, 'About.html')
+
+def clima(request):
+
+    return render(request)
+
+def twitter(request):
+
+    return(request)
