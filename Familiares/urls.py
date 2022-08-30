d34cb3c1d7c90ce5c8afc152a2b6b7531b16de67
Ethan Orozco <ethan.g.orozco@gmail.com>
2022-08-30 12:08:44 -0500

    segundo commit

--- Familiares/urls.py
+++ Familiares/urls.py
@@ -1,14 +1,13 @@
-from xml.etree.ElementInclude import include
 from django.urls import path
 from Familiares.views import about, familiares, inicio
 
 urlpatterns = [
     path('', inicio, name='FamiliaresInicio'),
     path('info/', familiares, name='FamiliaresInfo'),
-    path('about/', about, name= 'About'),
+    path('about/', about, name= 'About'),   
 
 ]
 
-urlpatterns = patterns('',
-    url(r'', include('external_urls.urls')), 
-)
+#urlpatterns = patterns('',
+#    url(r'', include('external_urls.urls')), 
+#)
