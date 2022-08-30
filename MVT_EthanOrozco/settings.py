344011ca7f5305a330688c0913d30967d45bdccc
Ethan Orozco <ethan.g.orozco@gmail.com>
2022-08-30 12:11:06 -0500

    tercer commit

--- MVT_EthanOrozco/settings.py
+++ MVT_EthanOrozco/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'external_urls',
+    #'external_urls',
     'Familiares'
 ]
 
